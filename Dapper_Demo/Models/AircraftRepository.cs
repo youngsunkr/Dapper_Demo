e89073a62edbcb4ce0e88a28a9894474a51eb4fe
youngsunkr <youngsunkr@gmail.com>
2018-09-12 02:43:50 +0900

    코드 수정중

---
+++ Dapper_Demo/Models/AircraftRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper_Demo.Models
+{
+    public class AircraftRepository
+    {
+        private string connectionString = DbConn.GetConnectionString();
+
+    }
+}
---
+++ Dapper_Demo/Models/AuthorRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Dapper_Demo.Models
+{
+    public class AuthorRepository
+    {
+        private string connectionString = DbConn.GetConnectionString();
+
+        public List<Author> ReadAll()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<Author>("Select * FROM Author").ToList();
+            }
+        }
+
+        public Author Find(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<Author>("Select * From Author WHERE Id = @Id", new { id }).SingleOrDefault();
+            }
+        }
+
+        public int Update(Author author)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "UPDATE Author SET FirstName = @FirstName, " +
+                                  " LastName = @LastName " + "WHERE Id = @Id";
+                int rowsAffected = db.Execute(sqlQuery, author);
+                return rowsAffected;
+            }
+        }
+
+        public List<Author> Read()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                string readSp = "GetAllAuthors";
+                return db.Query<Author>(readSp, commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+    }
+}
